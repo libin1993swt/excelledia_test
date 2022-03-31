989fa729c25a61a1f77f86e9d0a41995e6fc60cc
Libin Abraham <libinabraham1993@gmail.com>
2022-03-31 21:03:07 +0530

    Set parent_id nullable

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -28,7 +28,6 @@
                         <div class="form-group">
                             <input type="text" name="comment_body" class="form-control" />
                             <input type="hidden" name="post_id" value="{{ $post->id }}" />
-                            <input type="hidden" name="comment_id" value="{{ $comment->id ?? 0 }}" />
                         </div>
                         <div class="form-group">
                             <input type="submit" class="btn btn-warning" value="Add Comment" />
